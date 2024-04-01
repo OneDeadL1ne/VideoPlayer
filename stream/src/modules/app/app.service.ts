d4fb31599aefa7983192f2babb695f31b831c1b6
OneDeadL1ne <119927472+OneDeadL1ne@users.noreply.github.com>
2024-04-02 00:35:06 +0300

    create stream

---
+++ stream/src/modules/app/app.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  getHello(): string {
+    return 'Hello World!';
+  }
+  postHello(hello:string):string{
+    return hello
+  }
+}
