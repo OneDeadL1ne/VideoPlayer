c234817d07d4cee29e2afb8c1fd3ec0b09d1f1e0
OneDeadL1ne <119927472+OneDeadL1ne@users.noreply.github.com>
2024-05-10 19:00:12 +0300

    fix

--- back/src/config/config.js
+++ back/src/config/config.js
@@ -4,7 +4,7 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    port: 5432,
+    port: process.DB_PORT,
     dialect: process.env.DIALECT,
     dialectOptions: {
       bigNumberStrings: true,
